63bb0e84608cf29331bceb88e777e6735aa44dea
oborenkov <[oborenkov@yahoo.com]>
2023-04-16 21:46:18 +0200

    Homework_2

---
+++ Homework/Homework_2/Program.cs
@@ -0,0 +1,86 @@
+﻿
+// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+/*Console.WriteLine("Введите  натуральное трехзначное число и мы Вам покажем вторую цифру :");
+int num = Convert.ToInt32(Console.ReadLine());
+
+ if(num>100 && num<1000){
+Console.WriteLine("Вы ввели  корректное число");
+}
+else
+{
+    Console.WriteLine("Вы ввели не корректное число повторите попытку " );
+    return;
+}
+int SecondNum( int num)
+{
+     int second = num/10%10;
+    return second;
+}
+
+Console.WriteLine("Ваша вторая цифра  " + SecondNum(num));
+*/
+ 
+
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+    /*int ThirdDigit(int number)
+        {
+            int result = -1;
+            if (number >= 100)
+            {
+                while (number > 999)
+                {
+                    number = number / 10;
+                }
+                result = number % 10;
+            }
+            return result; 
+        }
+        Console.Write("Введите число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+ 
+if (ThirdDigit(number1) == -1)
+Console.WriteLine("третьей цифры нет");
+else
+Console.WriteLine($"Ваша третия цифра {ThirdDigit(number1)}");
+*/
+
+
+
+
+
+
+
+
+
+
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+
+/*Console.Write("Введи цифру, обозначающую день недели: ");
+int day = Convert.ToInt32(Console.ReadLine());
+bool dayNumber= Convert.ToBoolean(day);
+bool DayOff(bool dayNumber){
+  if(day > 5 && day < 8){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+Console.WriteLine(DayOff(dayNumber));
+*/
+
+
+
---
+++ Seminar/Seminar_2/Program.cs
@@ -0,0 +1,88 @@
+﻿// напишите программу ,которая выводит случайное трехзначное число и удаляет вторую цифру
+
+/*int randNamber= new Random().Next(100,1000); // случайная генирация числа
+
+int CutNumber(int num) {
+
+    int hunders =num / 100;
+    int units = num %  10;
+
+    int result = hunders * 10 + units;
+    return result;
+}
+ 
+ int newNumber= CutNumber(randNamber);
+ 
+ Console.WriteLine($"New version of {randNamber} is {newNumber}");
+ */
+
+
+
+//Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+/*
+int MaxDiget(int num){
+    int tens = num / 10;
+    int units = num % 10;
+    int max = 0;
+    if(tens>units){
+        tens = max;
+    }
+    else{
+        max = units;
+    }
+    return max;
+}
+int randNamber= new Random().Next(10,100);
+int newNumber = MaxDiget(randNamber);
+Console.WriteLine($"Largest digit of a number {randNamber} is {newNumber}");
+*/
+
+
+
+
+
+
+
+//Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно a и b.
+/*
+Console.WriteLine("Введите число и мы узнаем является ли оно кратным случайных двух чисел ");
+ int num = Convert.ToInt32(Console.ReadLine());
+ int a = new Random().Next(10,100);//Convert.ToInt32(Console.ReadLine()); можно запросить у пользователя
+ int b = new Random().Next(10,100);//Convert.ToInt32(Console.ReadLine()); можно запросить у пользователя
+
+ bool Multiple(int num,int a,int b){
+    if(num % a == 0 && num % b == 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+ }
+ Console.WriteLine("Cлучайное число " + a);
+ Console.WriteLine("Cлучайное число " + b);
+ Console.WriteLine(Multiple(num,a,b));
+ */
+
+
+ //Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+ 
+ Console.WriteLine("Введите превое число :");
+ int a = Convert.ToInt32(Console.ReadLine());
+  Console.WriteLine("Введите второе число и узнаем является ли оно квадратом первого :");
+ int b = Convert.ToInt32(Console.ReadLine());
+ bool Quadr(int a, int b){
+    if(a == b * b || b == a * a ){
+        return true;
+    }
+    else{
+        return false;
+    }
+ }
+Console.WriteLine(Quadr(a,b));
+
+
+
+
+
+
