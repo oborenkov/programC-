fc9eb8e596ce9352785eda92869954912a2ecd70
oborenkov <[oborenkov@yahoo.com]>
2023-04-24 00:11:45 +0200

    Homework_4

--- Homework/Homework_4/Program.cs
+++ Homework/Homework_4/Program.cs
@@ -1,2 +1,75 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Вы продлили срок сдачи задания до 24 апр., 10:00 +03:00 UTC
+
+
+// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+// int DegreeNun(int num1,int num2){
+//     int result=1;
+//     for(int i = 1;i<= num2;i++)
+//        result= result*num1;
+//         return result;
+// }
+// Console.Write("Введите число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите степень числа: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// int Degree = DegreeNun(a,b);
+// Console.WriteLine($" {a} в степени {b} = {Degree}" );
+
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+// Console.Write("Введите число: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+
+//   int SumNum(int number){
+    
+//     int counter = Convert.ToString(number).Length;
+//     int a = 0;
+//     int result = 0;
+
+//     for (int i = 0; i < counter; i++){
+//       a = number - number % 10;
+//       result = result + (number - a);
+//       number = number / 10;
+//     }
+//    return result;
+//   }
+
+// int sumNum = SumNum(num1);
+// Console.WriteLine("Сумма цифр в числе: " + sumNum);
+
+
+
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+// Console.WriteLine("Введите  число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// string Num = Convert.ToString(num);
+// int[] array= new int [Num.Length];
+// int i;
+// for(i=0;i<Num.Length;i++)
+// {
+// array[i]=Convert.ToInt32(Num[i].ToString());
+// Console.WriteLine(array[i] + "");
+// }
+
+
+
+
+
---
+++ Seminar/Seminar_4/Program.cs
@@ -0,0 +1,41 @@
+﻿//Напишите программу ,которая принемает на вход число (А) и выдает сумму чисел от 1 до А
+
+// int GetSum(int num){
+//     int sum = 0;
+//     for(int current = 1 ;current <= num;current++ )
+//         sum = sum + current;
+//         return sum;
+// }
+// Console.WriteLine( "Input a nunber: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// int res = GetSum(a);
+// Console.WriteLine($"Sum of nambers from 1 to {a} is {res}");
+
+ int[] CreateRandomArray(int size,int minValue,int maxVelue){
+    int[] array = new int[size];
+    for(int i= 0;i<size;i++ )
+       array[i]=new Random().Next(minValue,maxVelue + 1);
+       return array;
+ }
+ void ShowArray(int[]array){
+    for(int i =0;i<array.Length;i++)
+    Console.Write(array[i] + "  ");
+    Console.WriteLine();
+ }
+
+ Console.WriteLine("Input a size for array: ");
+ int size = Convert.ToInt32(Console.ReadLine());
+ Console.Write("Input a min possible value: ");
+ int  min = Convert.ToInt32(Console.ReadLine());
+ Console.Write("Input a max possible value: ");
+ int  max = Convert.ToInt32(Console.ReadLine());
+
+ int[] newArray = CreateRandomArray(size,min,max);
+ ShowArray(newArray);
+
+
+
+
+
+
+
