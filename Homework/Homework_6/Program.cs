7649732ae98fc5214f32fe1ed2505498bd1d9c7c
oborenkov <[oborenkov@yahoo.com]>
2023-04-30 23:43:02 +0200

    Homework_6

--- Homework/Homework_6/Program.cs
+++ Homework/Homework_6/Program.cs
@@ -8,8 +8,42 @@
 
 
 
+/*Console.Write("Введите элементы(через запятую): ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(","), int.Parse);
+int count = 0;
+ 
+for (int i = 0; i < arr.Length; i++)
+{
+	if (arr[i] > 0)
+	{
+		count++;
+	}
+}
+ 
+Console.WriteLine($"Кол-во элементов > 0: {count}");*/
 
 
 // Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+/*Console.Write("Введите k1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+ 
+double x = -(k1 - k2) / (b1 - b2);
+double y = b1 * x + k1;
+ 
+x = Math.Round(x, 3);
+y = Math.Round(y, 3);
+ 
+if(k1 == k2)
+         Console.Write("Прямые являются параллельными.");
+    else
+        Console.WriteLine($"Пересечение в точке: ({x};{y})");*/
+
