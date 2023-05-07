868a042aabbe86592a5828290d79cdd21302a534
oborenkov <[oborenkov@yahoo.com]>
2023-05-08 01:55:28 +0200

    Homework_8

--- Homework/Homework_8/Program.cs
+++ Homework/Homework_8/Program.cs
@@ -11,6 +11,64 @@
 // 9 5 3 2
 // 8 4 4 2
 
+/*Console.Write("Введите размерность m массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите размерность n массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] randomArray = new int[m,n];
+
+void mas(int m, int n)
+{
+int i,j;
+Random rand = new Random();
+  for (i = 0; i < m; i++)
+ {
+     for (j = 0; j < n; j++)
+   {
+       randomArray[i,j] = rand.Next(1,9);
+   }
+ }
+}
+
+void printm(int[,] array)
+{
+int i,j;
+for (i = 0; i < array.GetLength(0); i++)
+{
+Console.WriteLine();
+for (j = 0; j < array.GetLength(1); j++)
+{
+Console.Write($"{array[i,j]} ");
+}
+Console.WriteLine();
+}
+
+}
+
+mas(m,n);
+Console.WriteLine("Исходный массив: ");
+printm(randomArray);
+
+void streamline(int[,]arrey){
+    for (int i = 0; i < arrey.GetLength(0); i++)
+    {
+        for (int j = 0; j < arrey.GetLength(1); j++)
+        {
+            for (int b = 0; b < arrey.GetLength(1) -1; b++)
+            {
+                if(arrey[i,b]<arrey[i,b+1]){
+                    int upor = arrey[i,b +1];
+                    arrey[i,b +1]=arrey[i,b];
+                    arrey[i,b]=upor;
+                }
+            }
+        }
+    }
+}
+streamline(randomArray);
+Console.WriteLine("Отсортированный массив: ");
+printm(randomArray);*/
+
 
 // Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
 
@@ -25,6 +83,65 @@
 // 5 2 6 7
 // Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
 
+/*Console.Write("Введите размерность m массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = m;
+int[,] randomArray = new int[m,n];
+
+void mas(int m, int n)
+{
+int i,j;
+Random rand = new Random();
+for (i = 0; i < m; i++)
+{
+for (j = 0; j < n; j++)
+{
+randomArray[i,j] = rand.Next(1,9);
+}
+}
+}
+
+void printm(int[,] array)
+{
+int i,j;
+for (i = 0; i < array.GetLength(0); i++)
+{
+Console.WriteLine();
+for (j = 0; j < array.GetLength(1); j++)
+{
+Console.Write($"{array[i,j]} ");
+}
+Console.WriteLine();
+}
+}
+
+mas(m,n);
+Console.WriteLine("Исходный массив: ");
+printm(randomArray);
+
+
+int SumLine(int[,] array, int i)
+{
+int sum = array[i,0];
+for (int j = 1; j < array.GetLength(1); j++)
+{
+sum += array[i,j];
+}
+return sum;
+}
+
+int minSum = 1;
+int sum = SumLine(randomArray, 0);
+for (int i = 1; i < randomArray.GetLength(0); i++)
+{
+if (sum > SumLine(randomArray, i))
+{
+sum = SumLine(randomArray, i);
+minSum = i+1;
+}
+}
+Console.WriteLine($" Строка c наименьшей суммой элементов: {minSum}");*/
+
 
 
 // Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
@@ -34,6 +151,73 @@
 // Результирующая матрица будет:
 // 18 20
 // 15 18
+/*int m = InputNumbers("Введите число строк 1-й матрицы: ");
+int n = InputNumbers("Введите число столбцов 1-й матрицы (и строк 2-й): ");
+int p = InputNumbers("Введите число столбцов 2-й матрицы: ");
+int range = InputNumbers("Введите диапазон случайных чисел: от 1 до ");
+
+int[,] firstMartrix = new int[m, n];
+CreateArray(firstMartrix);
+Console.WriteLine($"Первая матрица:");
+WriteArray(firstMartrix);
+
+int[,] secomdMartrix = new int[n, p];
+CreateArray(secomdMartrix);
+Console.WriteLine($"Вторая матрица:");
+WriteArray(secomdMartrix);
+
+int[,] resultMatrix = new int[m,p];
+
+MultiplyMatrix(firstMartrix, secomdMartrix, resultMatrix);
+Console.WriteLine($"Произведение первой и второй матриц:");
+WriteArray(resultMatrix);
+
+void MultiplyMatrix(int[,] firstMartrix, int[,] secomdMartrix, int[,] resultMatrix)
+{
+  for (int i = 0; i < resultMatrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < resultMatrix.GetLength(1); j++)
+    {
+      int sum = 0;
+      for (int k = 0; k < firstMartrix.GetLength(1); k++)
+      {
+        sum += firstMartrix[i,k] * secomdMartrix[k,j];
+      }
+      resultMatrix[i,j] = sum;
+    }
+  }
+}
+
+int InputNumbers(string input)
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+
+void CreateArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i, j] = new Random().Next(range);
+    }
+  }
+}
+
+void WriteArray (int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write(array[i,j] + " ");
+    }
+    Console.WriteLine();
+  }
+}*/
+  
 
 
 
@@ -50,9 +234,52 @@
 
 
 
+
+
+
 // Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
 // Например, на выходе получается вот такой массив:
 // 01 02 03 04
 // 12 13 14 05
 // 11 16 15 06
 // 10 09 08 07
+
+
+/*int n = 4;
+int[,] spiral = new int[n, n];
+
+int temp = 1;
+int i = 0;
+int j = 0;
+
+while (temp <= spiral.GetLength(0) * spiral.GetLength(1))
+{
+  spiral[i, j] = temp;
+  temp++;
+  if (i <= j + 1 && i + j < spiral.GetLength(1) - 1)
+    j++;
+  else if (i < j && i + j >= spiral.GetLength(0) - 1)
+    i++;
+  else if (i >= j && i + j > spiral.GetLength(1) - 1)
+    j--;
+  else
+    i--;
+}
+
+WriteArray(spiral);
+
+void WriteArray (int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      if (array[i,j] / 10 <= 0)
+      Console.Write($" {array[i,j]} ");
+
+      else Console.Write($"{array[i,j]} ");
+    }
+    Console.WriteLine();
+  }
+}
+*/
--- Lecture/lecture_4/Program.cs
+++ Lecture/lecture_4/Program.cs
@@ -1,2 +1,123 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// string[,]table = new string[2,5];
+// String.Empty
+// table[0,0],table[0,1],table[0,2],table[0,3]
+// table[1,0][1,1][1,2]
+// int[,]matrix= new int[5,8];
+// table[1,2]= "слово";
+// for (int rows = 0; rows < 2; rows++)
+// {
+//     for (int columns = 0; columns < 5; columns++)
+//     {
+//        Console.Write($"{table[rows,columns]}-"); 
+//     }
+//     Console.WriteLine();
+// }
+
+// void PrintArrey(int[,]matr){
+
+// for (int i = 0; i < matr.GetLength(0); i++)
+// {
+//     for (int j = 0; j < matr.GetLength(1); j++)
+//     {
+//        Console.Write($"{matr[i,j]}"); 
+//     }
+//     Console.WriteLine();
+// }
+// }
+// void FillArrey(int[,]matr){
+//     for (int i = 0; i < matr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matr.GetLength(1); j++)
+//         {
+//             matr[i,j]= new Random().Next(1,10);
+//         }
+        
+//     }
+// }
+// int[,]matrix= new int[5,8];
+// PrintArrey(matrix);
+// FillArrey(matrix);
+// Console.WriteLine();
+// PrintArrey(matrix);
+
+//   int[,]pic = new int[,]
+// {
+//     {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+//     {0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0},
+//     {0,0,0,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,0,0},
+//     {0,0,0,0,0,0,0,0,0,0,1,1,0,0,1,0,0,0,0,0,0,0,0,0,0},
+//     {0,0,0,0,0,0,0,0,1,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0},
+//     {0,0,0,0,0,0,1,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0},
+//     {0,0,0,1,1,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0},
+//     {0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0},
+//     {0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0},
+//     {0,0,0,1,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,0},
+//     {0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0},
+//     {0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0},
+//     {0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,0},
+//     {0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0},
+//     {0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0},
+//     {0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,0},
+//     {0,0,0,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0},
+//     {0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,1,0,0,0,0,0},
+//     {0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,0,0,0,0,0,0},
+//     {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+//     {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+//     {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+//      {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+
+
+// };
+
+
+// void PrintImage(int[,] img){
+//     for (int i = 0; i < img.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < img.GetLength(1); j++)
+//         {
+//           if(img[i,j]==0) Console.Write($" ");
+//           else Console.Write($"+");
+//         }
+//     }
+// };
+// PrintImage(pic);
+// void FilldImage(int row , int col){
+//     if(pic[row,col]== 0){
+//         pic[row,col]=1;
+//         FilldImage(row -1,col);
+//           FilldImage(row,col -1);
+//             FilldImage(row +1,col);
+//               FilldImage(row ,col +1);
+//     }
+
+// };
+// PrintImage(pic);
+// FilldImage(13,13);
+// PrintImage(pic);
+
+//1!=1;0!=1
+// double Factorial(int n){
+//     if(n==1) return 1;
+//       else return n * Factorial(n - 1); 
+// }
+// for (int i = 1; i < 40; i++)
+// {
+//  Console.WriteLine($"{i}!= {Factorial(i)}");
+
+// }
+//  //Console.WriteLine(Factorial(i));
+
+//фибоначи
+
+// f(1)=1
+// f(2)=1
+// f(n)=f(n - 1) + f(n-2)
+// double Fibonacci(int n){
+//   if (n == 1|| n==2) return 1;
+//   else return Fibonacci(n-1) + Fibonacci(n-2);
+  
+// }
+// for (int i = 1; i < 40; i++)
+// {
+//   Console.WriteLine(Fibonacci(i));
+// }
